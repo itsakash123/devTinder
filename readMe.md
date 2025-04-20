a62cea10ab251e42dfa96a91db6e6d693c046658
itsakash123 <akashkumar2895555@gmail.com>
2025-04-21 05:06:42 +0530

    exploring get post delete

---
+++ readMe.md
@@ -0,0 +1,5 @@
+whenever you type some url you are making get API call to this route on our server ex https://localhost3000/xyz
+
+for testing our API broswer is not a good way there is a software Postman
+
+install postman app and make a workspace/collection->test API call
--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
 const express=require("express");
 
 const app=express();
-// app.use("/",(req,res)=>{
-//     res.send("hello from dashboard")
-// })
-app.use("/", (req, res) => {
-  res.send("hello namaste from the dashboard");
-});
-app.use("/hello", (req, res) => {
-  res.send("hello hello hello");
-});
-app.use("/test",(req,res)=>{
-    res.send("hello from the server")
-})
+app.use(express.json());
 
+// //order of these routes matter a lot
+// app.use("/hello", (req, res) => {
+//   res.send("hello hello hello");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("hello namaste from the dashboard");
+// });
+
+//this method will match only  GET call  to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akash", lastName: "Kumar" });
+});
+// app.use("/user",(req,res)=>{
+//   res.send("Hahaha")
+// })
+app.post("/user", (req, res) => {
+  console.log("Save data to the database");
+  res.send("Data successfully saved to the database");
+});
+
+//This will match all the HTTP method API call to /test
+app.use("/test", (req, res) => {
+  res.send("hello from the server");
+});
+app.delete("/user",(req,res)=>{
+  res.send("Data deleted successfully")
+})
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000....")
